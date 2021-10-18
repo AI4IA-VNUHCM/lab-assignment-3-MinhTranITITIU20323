6ced923ec6950d9537cd0dc4f9b8e067da73fbaa
MinhTranIU2410 <ITITIU20323@student.hcmiu.edu.vn>
2021-10-18 17:47:11 +0700

    Exercise 5

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -10,11 +10,30 @@ ______________________________________
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+void swap(int *s1, int *s2)
+{
+    int temp = *s1;
+    *s1 = *s2;
+    *s2 = temp;
+}
 
 void Ex5(int arr[], int n){
 	//Your codes here
-	
+    int i=0,j=0,temp;
+    for (i=0; i<n; i++){
+
+        if (arr[i] > 0){
+            if (i != j){
+                swap(&arr[i], &arr[j]);
+            }
+            j++;
+        }
+    }
+	for(i=0;i<n;i++){
+		printf("%d ",arr[i]);
+	}
 }
+
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
