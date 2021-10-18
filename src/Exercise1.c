8bec8d5a5fa595f3953f5f597716f6416f76219e
MinhTranIU2410 <ITITIU20323@student.hcmiu.edu.vn>
2021-10-18 17:12:27 +0700

    Exercise 1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -14,7 +14,17 @@ Ex:
 
 void Ex1(int arr[], int n){
 	//Your codes here
-    
+    int i,j;
+	for (i = 0, j = n-1; i < j; i++, j--) {
+    if (arr[i] != arr[j]) {
+        printf("not symmetric");
+        break;
+    }
+	else{
+		printf("symmetric");
+		break;
+	}
+	}
 }
 
 int main(int argc, char *argv[]) {
