15d00478dfcec817a627a77ea8631d95ef957a9c
MinhTranIU2410 <ITITIU20323@student.hcmiu.edu.vn>
2021-10-18 18:21:23 +0700

    Exercise 5

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -19,23 +19,28 @@ void swap(int *s1, int *s2)
 
 void Ex5(int arr[], int n){
 	//Your codes here
-    int i=0,j=0,temp;
-    for (i=0; i<n; i++){
-
-        if (arr[i] > 0){
-            if (i != j){
-                swap(&arr[i], &arr[j]);
-            }
-            j++;
-        }
-    }
+    long int i=0,over=0,under=0,arrOver[n],arrUnder[n];
 	for(i=0;i<n;i++){
-		printf("%d ",arr[i]);
+		if(arr[i]>0){
+			arrOver[over]=arr[i];
+			over++;
+		}
+		else{
+			arrUnder[under]=arr[i];
+			under++;
+		}
+	}
+	for(i=0;i<over;i++){
+		printf("%d ",arrOver[i]);
+	}
+    for(i=0;i<under;i++){
+		printf("%d ",arrUnder[i]);
 	}
 }
 
 
-int main(int argc, char *argv[]) {
+
+long int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
 	int testcase[argc],i;
